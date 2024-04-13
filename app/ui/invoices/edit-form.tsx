812195cb5c4f37dd8b1ba916039094cc1f2c14a7
Shintaro Manako <shintaro.manako@accenture.com>
2024-04-13 22:49:08 +0900

    error handling and auth

---
+++ app/ui/invoices/edit-form.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+export default function EditInvoiceForm({
+  invoice,
+  customers,
+}: {
+  invoice: InvoiceForm;
+  customers: CustomerField[];
+}) {
+  const initialState = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+
+  return <form action={dispatch}></form>;
+}
